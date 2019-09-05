fb0ef6732fa69d55650e720c0c8519734a62c1f6
Student <example@example.com>
2019-09-05 16:13:47 -0700

    working haiku checker, does not account for y as a vowel so 'lovely' counts the o and e

--- src/main.js
+++ src/main.js
@@ -6,36 +6,53 @@ import './styles.css';
 const countVowels = function(arr){
   const vowelTest = /[aeiouAEIOU]/;
   const consonantTest = /[a-zA-Z^AEIOUaeiou]/;
-  let numVowels = 0;
+  let numSyllables = 0;
+  let flag = false;
   for(let i = 0; i < arr.length; i++){
     if(vowelTest.test(arr[i])){
-      numVowels++;
+      numSyllables++;
     }
   }
   let j = arr.length;
   if(j > 2){
+    //check if last letter is vowel, second to last is consonant, third to last vowel
     if(vowelTest.test(arr[j-1]) && consonantTest.test(arr[j-2]) && vowelTest.test(arr[j-3])){
-      numVowels--;
+      numSyllables--;
+      flag = true;
     }
     for(let i = 1; i < arr.length; i++){
+      //check if 2 vowels in a row
       if(vowelTest.test(arr[i]) && vowelTest.test(arr[i-1])){
-        numVowels--;
+        numSyllables--;
       }
     }
   }
-
+  //scarves and yes are 1 syllable
+  if(j > 3 && flag === false){
+    if(arr[j-1] === "e" || (arr[j-2] + arr[j-1]) === "es"){
+      numSyllables--;
+    }
+  }
+  return numSyllables;
 }
 
 const haikuChecker = function(haiku){
+  let syllableCount = [0,0,0];
   for(let i = 0; i < haiku.length; i++){
     for(let j = 0; j < haiku[i].length; j++){
       let word = haiku[i][j].split("");
       let numSyllables = countVowels(word);
-      console.log(numSyllables);
+      syllableCount[i] += numSyllables;
     }
   }
+  return syllableCount;
 }
-
+//Scarves and sweaters everywhere
 $(document).ready(function(){
-  haikuChecker([["Coolness fills the air"],["Scarves and sweaters everywhere"],["Fall weather is here"]]);
+  let findSyllables = haikuChecker([["Coolness", "fills", "the", "air"],["Scarves", "and", "sweaters", "everywhere"],["Fall", "weather", "is", "here"]]);
+  console.log(findSyllables);
+   findSyllables = haikuChecker([["An", "old", "silent", "pond"],["A", "frog", "jumps", "into", "the", "pond"],["splash", "silence", "again"]]);
+  console.log(findSyllables);
+   findSyllables = haikuChecker([["In", "the", "twilight", "rain"],["these", "brilliant", "hued", "hibiscus"],["A", "lovely", "sunset"]]);
+  console.log(findSyllables);
 });
